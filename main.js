6e15c76844e4401e04af12e1cc3c86e928d32438
Henrique Ferreira <105758892+NOSC4P3@users.noreply.github.com>
2022-05-17 23:29:31 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const linksSocialMedia = {
+  facebook: 'maykebrito',
+  youtube: 'jakelinygracielly',
+  instagram: 'henriqueferreira.svg',
+  twitter: 'NOSC4P3',
+  github: 'NOSC4P3'
+}
+function changeSocialMediaLinks() {
+  for (let li of socialMedias.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+changeSocialMediaLinks()
+//API GITHUB
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userLogin.textContent = data.login
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGithubProfileInfos()
